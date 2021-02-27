168bbf28e95ee109a5b1215747db5ff071d5cb45
Daria <azuca25@gmail.com>
2021-02-27 19:43:08 +0300

    Second commit of DIV

---
+++ calc.py
@@ -0,0 +1,26 @@
+str_input = input("Delimoe: ")
+delimoe = int(str_input)
+#print(type(delimoe))
+
+operation = input("+ / * - ^: ")
+
+str_input2 = input("Delitel: ")
+
+delitel = int(str_input2)
+#print(type(delitel))
+
+if operation == '/':
+ result = delimoe / delitel
+elif operation == '+':
+ result = delimoe + delitel
+elif operation == '-':
+ result = delimoe - delitel
+elif operation == '*':
+ result = delimoe * delitel
+elif operation == '^':
+ result = delimoe ** delitel
+else:
+ result = "unknown"
+#print(type(result))
+
+print("Result: " + str(result))
---
+++ test.py
@@ -0,0 +1,12 @@
+str_input = input("Delimoe: ")
+delimoe = int(str_input)
+print(type(delimoe))
+
+str_input2 = input("Delitel: ")
+delitel = int(str_input2)
+print(type(delitel))
+
+result = delimoe / delitel
+#result = str_input / str_input2
+
+print(result)
