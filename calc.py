988c5bd0b6affe8ee3c9729b7b5b88e21d51c4b6
Daria <azuca25@gmail.com>
2021-03-11 14:58:50 +0300

    third commit of DIV

--- calc.py
+++ calc.py
@@ -1,16 +1,55 @@
-str_input = input("Delimoe: ")
-delimoe = int(str_input)
+#simple div
+
+str_command = input("Please type command a + b or a-b")
+
+sign_A = ''
+str_A = ''
+
+sign_B = ''
+str_B = ''
+
+operation = ''
+i = 0
+
+while i < len(str_command):
+	if str_command[i] == '+' or str_command[i] == '-' or str_command[i] == '*' or str_command[i] == '/' or str_command[i] == '^':
+		if str_A == '':
+			sign_A = str_command[i]
+		elif operation != '':
+			sign_B = str_command[i]
+		else:
+			operation = str_command[i]
+	else:
+		if operation == '':
+			str_A += str_command[i]
+		else:
+			str_B += str_command[i]
+	i += 1
+
+str_A = sign_A + str_A.strip()
+str_B = sign_B + str_B.strip()
+print(str_A)
+print(str_B)
+
+
+#str_input = input("A: ")
+
+delimoe = float(str_A)
 #print(type(delimoe))
 
-operation = input("+ / * - ^: ")
+#operation = input("+ / * - ^: ")
 
-str_input2 = input("Delitel: ")
-
-delitel = int(str_input2)
+#str_input2 = input("B: ")
+delitel = float(str_B)
 #print(type(delitel))
+result = None
 
 if operation == '/':
- result = delimoe / delitel
+	if delitel == 0:  
+		result = 'Inf'
+	else:
+		result = delimoe / delitel
+#print(type(result))
 elif operation == '+':
  result = delimoe + delitel
 elif operation == '-':
@@ -19,9 +58,11 @@ elif operation == '*':
  result = delimoe * delitel
 elif operation == '^':
  result = delimoe ** delitel
+
 else:
  result = "unknown"
 #lol
 #print(type(result))
 
+print("Result: " + str(result))
 print("Result: " + str(result))
