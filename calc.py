8b892dbd69b6f30823c251f1dd1096630e3ba41e
Daria <azuca25@gmail.com>
2021-03-19 22:04:24 +0300

    calc march

--- calc.py
+++ calc.py
@@ -1,68 +1,60 @@
-#simple div
+# calc march
 
-str_command = input("Please type command a + b or a-b")
+str_command = input("Please type command a + b or a-b: ")
+# str_command = input()
+str_command.replace(' ', '')
 
-sign_A = ''
+'''
+parsing
+'''
+
+hp_ops = tuple('^')
+mp_ops = tuple('*/')
+lp_ops = ('+', '-')
+supported_ops = hp_ops + mp_ops + lp_ops
+digits_chars = tuple(map(str, range(10))) + tuple('.,-')
+
 str_A = ''
-
-sign_B = ''
 str_B = ''
+actions = []
 
-operation = ''
-i = 0
+d = dict()
+d['opr'] = 'None'
+d['val'] = ''
+actions.append(d)
 
-while i < len(str_command):
-	if str_command[i] == '+' or str_command[i] == '-' or str_command[i] == '*' or str_command[i] == '/' or str_command[i] == '^':
-		if str_A == '':
-			sign_A = str_command[i]
-		elif operation != '':
-			sign_B = str_command[i]
-		else:
-			operation = str_command[i]
-	else:
-		if operation == '':
-			str_A += str_command[i]
-		else:
-			str_B += str_command[i]
-	i += 1
+for i, letter in enumerate(str_command):
+	if letter in supported_ops and (i > 0) and actions[-1].get('val') != '':
+		actions.append({'opr': letter,
+						'val': ''})
+	elif letter in digits_chars:
+		actions[-1]['val'] = actions[-1].get('val') + letter
 
-str_A = sign_A + str_A.strip()
-str_B = sign_B + str_B.strip()
-print(str_A)
-print(str_B)
+'''
+calculation
+'''
+#variables = list(map(float, variables))
+for action in actions:
+	action['val'] = float(action.get('val'))
 
-
-#str_input = input("A: ")
-
-delimoe = float(str_A)
-#print(type(delimoe))
-
-#operation = input("+ / * - ^: ")
-
-#str_input2 = input("B: ")
-delitel = float(str_B)
-#print(type(delitel))
 result = None
 
-if operation == '/':
-	if delitel == 0:  
-		result = 'Inf'
+for action in actions:
+	if type(result) == str:
+		break
+
+	var_A = result
+	var_B = action.get('val')
+	operation = action.get('opr')
+
+	if operation in '+-*/':
+		if var_B == 0 and operation == '/':
+			result = 'Inf'
+		else:
+			result = eval('{0}{1}{2}'.format(var_A, operation, var_B))
+	elif operation == '^':
+		result = var_A ** var_B
 	else:
-		result = delimoe / delitel
-#print(type(result))
-elif operation == '+':
- result = delimoe + delitel
-elif operation == '-':
- result = delimoe - delitel
-elif operation == '*':
- result = delimoe * delitel
-elif operation == '^':
- result = delimoe ** delitel
+		result = var_B
 
-else:
- result = "unknown"
-#lol
-#print(type(result))
-
-print("Result: " + str(result))
 print("Result: " + str(result))
