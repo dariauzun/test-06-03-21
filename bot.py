f069e9da9e5fb4359e7725e23dc152ec79791107
Daria <azuca25@gmail.com>
2021-06-05 01:25:36 +0300

    4-5/6

--- bot.py
+++ bot.py
@@ -19,30 +19,45 @@ bot = telebot.TeleBot('1883015533:AAGn0-6zRCyq9tJV0gCJ6kyj5-0l85dyT7c')
 @bot.message_handler(content_types=['text'])
 
 def start(message):
-	bot.send_message(message.from_user.id, 'Здравствуйте!')
+	bot.send_message(message.from_user.id, 'Здравствуйте, {message.from_user.first_name}!')
 	#bot.register_next_step_handler(message, data)
 
+def get_respond(message):
+	bot.send_chat_action(message.from_user.id, 'typing')
+	if message.text == "Здравствуйте":
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		key1 = types.KeyboardButton('Online события')
+		key2 = types.KeyboardButton('Offline события') 
+		markup.add(key1, key2)
+
+@bot.message_handler(commands=['info'])
+def send_welcome(message):
+        name = bot.get_me()
+        print(name)
+        bot.reply_to(message, "Welcome")
+        
 with open('gazdata.txt','r') as f:
 	lines = f.readlines()
-    
-class Event(object):
+
+class MyEvent(object):
     event_name = 'Как настроить логистику для вашего бизнеса'
-    event_date = datetime.date(2021, 5, 27)
+    event_date = datetime.date(2021, 6, 5)
     
-    def  _init_(self, line):
-        self.event_name - line.split('')[0]
+    def __init__(self, line):
+        self.event_name = line.split('')[0]
         pass
-        
+
 events = []
 for l in lines:
     #'Как настроить логистику для вашего бизнеса', '2021, 5, 27'
-    events.append(Event()) #['Как настроить логистику для вашего бизнеса', '27/05']
+    events.append(MyEvent(l)) #['Как настроить логистику для вашего бизнеса', '27/05']
     
 class MyUser(object):
-    name = ''
+    name = '@uzunishe'
     user_events = []
     
-my_users = {'uzunishe': MyUser}
+my_users = {'tg_name': MyUser}
 for l in lines:
     pass
-bot.polling()
+
+bot.polling(none_stop=True)
